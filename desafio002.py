ccc4fea1b929f084bedea07cb8477a57ed4f573b
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-06-30 10:44:37 -0300

    Add files via upload

---
+++ desafio002.py
@@ -0,0 +1,6 @@
+#Crie um script que leia o dia, o mês e o ano do usuuário e mostre uma mensagem com a data formatada
+
+dia = input("Qual o dia o seu nascimento? ")
+mes = input("Qual o mês do seu nascimento? ")
+ano = input("Qual o ano do seu nascimento? ")
+print("Você nasceu em", dia + " de " +mes+" de " +ano+ ". Correto?")
