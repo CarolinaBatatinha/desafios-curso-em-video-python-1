005b1305846f09e610f6cf4593cb4c97fc5e1a2c
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-06-30 10:45:23 -0300

    Add files via upload

---
+++ desafio003.py
@@ -0,0 +1,6 @@
+#Criar um script que leia dois números e tente mostrar a soma entre eles
+
+n1 = int(input("Digite um número inteiro: "))
+n2 = int(input("Digite outro número inteiro: "))
+soma = n1+n2
+print('O valor da soma entre {} e {} é igual a {}' .format(n1, n2, soma))
