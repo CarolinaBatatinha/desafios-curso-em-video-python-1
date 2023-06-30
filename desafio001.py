367dd236c0607d2e1154c1ccd6b898c78f7d5102
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-06-30 10:44:21 -0300

    Add files via upload

---
+++ desafio001.py
@@ -0,0 +1,4 @@
+#Criar um script que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado.
+
+nome = input('Qual eh o seu nome? ')
+print(f'Olá, {nome}. Welcome, viu?')
