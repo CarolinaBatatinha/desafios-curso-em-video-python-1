689407131f06ad0b57f935b5d0360b30c87117b6
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-03 09:48:07 -0300

    Add files via upload

---
+++ desafio014.py
@@ -0,0 +1,4 @@
+#Conversor de temperatura
+cel = float(input('Qual a temperatura em ° C? '))
+far = (9 * cel)/5 + 32
+input('A temperatura em celsius é de {}° C. \nEla corresponde a {}° F.'.format(cel, far))
