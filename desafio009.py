a64651e07e5e6e90ed1f4cf83c4dfcd7ba35d1c3
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-03 09:47:17 -0300

    Add files via upload

---
+++ desafio009.py
@@ -0,0 +1,3 @@
+#Fazer um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+n = int(input('Digite um número inteiro: '))
+print('=== Tabuada de {} === \n{} x 0 = {}\n{} x 1 = {} \n{} x 2 = {} \n{} x 3 = {}'.format(n,n,n*0,n,n*1,n,n*2,n,n*3))
