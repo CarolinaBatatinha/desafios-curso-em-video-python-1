dd78f5bbc5b720c9eafec8f1c2692a6d08b3a98a
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-05 08:46:16 -0300

    Add files via upload

---
+++ desafio028.py
@@ -0,0 +1,11 @@
+# Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu
+
+from random import randint
+
+numComputador = randint(0,5)
+numUsuario = int(input('Digite um número inteiro entre 0 e 5: '))
+
+if numUsuario == numComputador:
+    print('Parabéns, você acertou o número sorteado.')
+else:
+    print('Você escolheu {0} e o computador escolheu {1}.'.format(numUsuario, numComputador))
