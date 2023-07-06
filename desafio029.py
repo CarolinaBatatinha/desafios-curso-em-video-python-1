925314621a4344eb2dd5d4ebf2c35691a6cfe7d1
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-06 10:41:06 -0300

    Add files via upload

---
+++ desafio029.py
@@ -0,0 +1,11 @@
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. A multa custará R$ 7,00 por cada Km acima do limite.
+
+vel = int(input('Informe a velocidade registrada em km/h: '))
+
+if vel > 80:
+    valorMulta = (vel - 80) * 7
+    print('A velocidade registrada foi de {0}km/h.')
+    print('Como o limite foi ultrapassado, você pagará uma multa de R$ {0:.2f}.'.format(valorMulta))
+else:
+    print('A velocidade registrada foi de {0}km/h.'.format(vel))
+    print('Continue de olho nos limites das vias.')
